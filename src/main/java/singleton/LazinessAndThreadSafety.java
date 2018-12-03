875838607a5d0c305a28c2a87a291f0c288302bc
sergiopoliveira <sergiopoliveira@outlook.com>
2018-12-03 22:25:57 +0000

    Laziness and Thread Safety

---
+++ src/main/java/singleton/LazinessAndThreadSafety.java
@@ -0,0 +1,33 @@
+package singleton;
+
+class LazySingleton {
+    private static LazySingleton instance;
+
+    private LazySingleton() {
+        System.out.println("Initializating a lazy singleton");
+    }
+
+//    public static synchronized LazySingleton getInstance() {
+//        if(instance == null) {
+//            instance = new LazySingleton();
+//        }
+//        return instance;
+//    }
+//}
+
+// double-checked locking
+    public static LazySingleton getInstance() {
+        if(instance == null) {
+            synchronized (LazySingleton.class) {
+                if(instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
+public class LazinessAndThreadSafety {
+
+
+}
