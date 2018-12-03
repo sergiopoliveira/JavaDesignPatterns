854bddaf950905ecea2fd1d88975affd6d2a14da
sergiopoliveira <sergiopoliveira@outlook.com>
2018-12-03 22:36:06 +0000

    Inner Static Singleton

--- src/main/java/singleton/LazinessAndThreadSafety.java
+++ src/main/java/singleton/LazinessAndThreadSafety.java
@@ -15,18 +15,42 @@ class LazySingleton {
 //    }
 //}
 
-// double-checked locking
+    // double-checked locking
     public static LazySingleton getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             synchronized (LazySingleton.class) {
-                if(instance == null) {
+                if (instance == null) {
                     instance = new LazySingleton();
                 }
             }
         }
         return instance;
     }
+
 }
+
+//  This is called the initialization-on-demand holder idiom.
+//  In Java, encapsulating classes do not automatically initialize inner classes.
+//  So the inner class only gets initialized by getInstance().
+//  Then again, class initialization is guaranteed to be sequential in Java,
+//  so the JVM implicitly renders it thread-safe.
+class InnerStaticSingleton {
+    private InnerStaticSingleton() {
+    }
+
+    private static class Impl {
+
+        // Inner classes can access private members of outer-classes
+        private static final InnerStaticSingleton INSTANCE
+                = new InnerStaticSingleton();
+    }
+
+    public InnerStaticSingleton getInstance() {
+        return Impl.INSTANCE;
+    }
+
+}
+
 public class LazinessAndThreadSafety {
 
 
